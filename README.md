ead598b650675b0f677437e8e076dad342be6cca
Alex <tat1212wong@gmail.com>
2021-02-23 14:07:19 +0800

    Improve docs

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lab 2 for 2021 Spring ISDN4000I
+
+## How to use this repository
+
+This repository contains the lab 2 skeleton code, which you should use as the basis for your work.
+
+This lab consists of 8 small tasks, you are encouraged to finish the tasks in order. Read the comments included in each task carefully, as it will guide you through the task.
+
+You should fork (create a copy of) this repository into your own GitHub account, and submit your work by committing to your forked repository. You can find the ```Fork``` button on the top right of the repository webpage.
+
+Clone your forked repository to your computer and you can start working on the lab.
+
+## Datasheet
+
+The focus of this lab is to write a driver for the on-board IMU (LSM6DS3) on the Arduino nano 33 IOT board. Writing the driver software will require careful reading of the LSM6DS3 datasheet.
+
+You can find the datasheet [here](https://www.st.com/resource/en/datasheet/lsm6ds3.pdf).
+
+The datasheet is quite long, but the comments given in the skeleton code will narrow down the pages you have to read on order to finish each task.
--- lab2skeleton/lab2skeleton.ino
+++ lab2skeleton/lab2skeleton.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 
 // Period between sensor data reading, in milliseconds
-#define SAMPLING_TIME       100              
+#define SAMPLING_TIME       50              
 
 /********************************* TASK 1 *********************************/
 // Refer to page 35 of the datasheet, what is the 7-bit i2c address of the IMU? 
@@ -39,7 +39,7 @@ imuDataScaled_t imuDataScaled;
 // devAddr      is the 7-bit i2c address of the IMU
 // regAddr      is the 8-bit address of the register to write into
 // data         is the byte of data to be written into the specified register
-// You will find these functions useful:    Wire.beginTransmission(byte)
+// You might find these functions useful:   Wire.beginTransmission(byte)
 //                                          Wire.write(const uint8_t *data, size_t quantity)
 //                                          Wire.endTransmission()
 void writeByteToRegister(byte devAddr, byte regAddr, byte data) {
@@ -53,7 +53,7 @@ void writeByteToRegister(byte devAddr, byte regAddr, byte data) {
 // regAddr      is the 8-bit address of the register to start reading from
 // data         is an array of bytes where the data read from the IMU will be stored into
 // length       is the amount of bytes to read from the IMU
-// You will find these functions useful:    Wire.beginTransmission(byte)
+// You might find these functions useful:   Wire.beginTransmission(byte)
 //                                          Wire.write(byte)
 //                                          Wire.endTransmission()
 //                                          Wire.requestFrom(uint8_t address, size_t quantity)
@@ -68,21 +68,21 @@ void setup() {
 
     /********************************* TASK 3 *********************************/
     // Write to CTRL1_XL register, so that the accelerometer starts operation, and
-    // is configered for 6.66kHz sampling rate, +-4g range and no filtering.
+    // is configured for 6.66kHz sampling rate, +-4g range and no filtering.
     // Refer to page 54 of the datasheet
     writeByteToRegister(IMU_I2C_ADDR, , );
 
     /********************************* TASK 4 *********************************/
     // Write to CTRL2_G register, so that the gyroscope starts operation, and
-    // is configered for 1.66kHz sampling rate and +-500dps.
+    // is configured for 1.66kHz sampling rate and +-500dps.
     // Refer to page 55 of the datasheet
     writeByteToRegister(IMU_I2C_ADDR, , );
 }
 
-unsigned long lastTime = 0;
-
 void loop() {
+    static unsigned long lastTime = 0;
     if (millis() - lastTime > SAMPLING_TIME) {
+        lastTime = millis();
         
         /********************************* TASK 6 *********************************/
         // Starting from register OUTX_L_G, read 12 bytes of data, so that data from
